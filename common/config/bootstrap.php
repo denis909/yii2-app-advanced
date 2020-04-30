33301ac8ce946a2bf113584c797048b733011296
denis909 <denis909@mail.ru>
2020-05-01 00:27:05 +0300

    n/a

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -5,6 +5,7 @@ Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 
+require dirname(dirname(__DIR__)) . '/vendor/denis909/yii2-censoring/config/bootstrap.php';
 require dirname(dirname(__DIR__)) . '/vendor/denis909/yii2-backend/config/bootstrap.php';
 require dirname(dirname(__DIR__)) . '/vendor/denis909/yii2-glide/config/bootstrap.php';
 require dirname(dirname(__DIR__)) . '/vendor/denis909/yii2-storage/config/bootstrap.php';
