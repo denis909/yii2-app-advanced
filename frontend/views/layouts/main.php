719a67236ca9caad780a43d3416dec3a9146c358
denis909 <denis909@mail.ru>
2020-05-02 21:24:38 +0300

    n/a

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -10,5 +10,17 @@ $theme = Yii::$app->theme;
 echo $theme->mainLayout([
     'content' => $content,
     'breadcrumbs' => ArrayHelper::getValue($this->params, 'breadcrumbs', []),
-    'actionMenu' => ArrayHelper::getValue($this->params, 'actionMenu', [])
+    'mainMenu' => ArrayHelper::getValue(Yii::$app->params, 'mainMenu', []),
+    'cart' => Yii::$app->shop->getCartItems(),
+    'cartOptions' => [
+        'emptyMessage' => Yii::t('app', 'Cart is empty.'),
+        'menu' => [
+            [
+                'label' => Yii::t('app', 'View Cart'), 
+                'url' => ['/shop/cart']
+            ]
+        ]
+    ],
+    'actionMenu' => ArrayHelper::getValue($this->params, 'actionMenu', []),
+    'userMenu' => !Yii::$app->user->isGuest ? ArrayHelper::getValue(Yii::$app->params, 'memberMenu', []) : [],
 ]);
